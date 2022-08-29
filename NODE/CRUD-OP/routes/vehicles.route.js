7d800e4919892f439178b4b867d316f8b0b8a8a3
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-08-29 19:23:55 +0530

    Adding crud

--- NODE/CRUD-OP/routes/vehicles.route.js
+++ NODE/CRUD-OP/routes/vehicles.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const VehicleController = require('../controller/vehicle.controller')
+const VehicleController = require('../controller/vehicle.controller');
 const router = express.Router();
 
-router.post('/create', VehicleController.createVehicle);
+router.post('/create-user', VehicleController.createVehicle);
+// router.get('/get-user', VehicleController);
+// router.put('/edit-user', VehicleController);
+// router.delete('/new-data', VehicleController);  
 
 module.exports = router;
