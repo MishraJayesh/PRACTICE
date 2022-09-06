9cce35652ea17a8977b826f0def9d9a25411b9ae
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-09-06 11:44:05 +0530

    Adding Insta Clone Home Page

---
+++ TEST/myapp/src/App.js
@@ -0,0 +1,13 @@
+import Clone from './pages/clone';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        {/* <Home /> */}
+        <Clone />
+      </div>
+  );
+}
+
+export default App;
---
+++ TEST/myapp/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+import Main from '../components/main';
+import Footer from '../components/footer';
+
+function Home() {
+    return(
+        <div>
+            <NavBar />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
