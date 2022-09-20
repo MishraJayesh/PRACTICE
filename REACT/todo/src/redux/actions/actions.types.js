8d90a0a39fb6e60c3a67a5fa0aca396784b0c6d5
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-09-20 11:57:00 +0530

    modified

--- REACT/todo/src/redux/actions/actions.types.js
+++ REACT/todo/src/redux/actions/actions.types.js
@@ -1,5 +1,5 @@
 export const ADD_COUNTER = "ADD_COUNTER";
-export const RESET_COUNTER = "ADD_COUNTER";
+export const RESET_COUNTER = "RESET_COUNTER";
 export const ADD_TODO = "ADD_TODO";
 export const CLEAR_TODO_LIST = "CLEAR_TODO_LIST";
 export const DELETE_TODO = "DELETE_TODO";
