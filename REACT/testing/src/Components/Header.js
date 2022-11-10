29997ec27e03ec000fb8799e0e6b11f151cbb488
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-11-10 14:32:41 +0530

    Adding Blog Site First Frontend Setup

---
+++ REACT/testing/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../Components/Assets/logo.png';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="img-container">
+                <img src={logo} alt=" logo" />
+            </div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/category">Category</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/faq">FAQs</a></li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Header;
