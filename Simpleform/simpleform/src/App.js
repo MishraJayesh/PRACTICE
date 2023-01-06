9b50ae45afae00ac2320e072b842aba37b9a8e4f
Jayesh Mishra <jm6125523@gmail.com>
2023-01-06 16:34:12 +0530

    mnr chng

--- Simpleform/simpleform/src/App.js
+++ Simpleform/simpleform/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 import './App.css';
 
@@ -10,7 +9,6 @@ function App() {
     Mobile: "91",
     Emails: " ",
     Password: " ",
-    ConfirmPassword: " ",
   });
 
   function handleChange(e) {
@@ -18,52 +16,27 @@ function App() {
     setForm({ ...form, [name]: value });
 
     let p = form.Password;
-    let cp = form.ConfirmPassword;
 
     if (p.length < 6) {
-      alert("Please make sure password is longer than 6 characters.")
+      alert("Password Should Be Of Maximum 6 Characters.")
       return false;
     }
-    if (p !== cp) {
-      alert("Please make sure passwords match.")
-      return false;
-    }
-
   }
 
   return (
     <div className="container">
-      <div className="brand-logo"></div>
       <div className="brand-title">JUST BLOGS...</div>
       <div className="inputs">
-        <FloatingLabel>
-          <label>NAME</label>
-          <input value={form.Name} name="Name" onChange={(e) => handleChange(e)} type="text" required />
-        </FloatingLabel>
-
-        <FloatingLabel>
-          <label>MOBILE</label>
-          <input value={form.Mobile} name="Mobile" onChange={(e) => handleChange(e)} type="number" placeholder="example@test.com" required />
-        </FloatingLabel>
-
-        <FloatingLabel>
-          <label>EMAIL</label>
-          <input value={form.Emails} name="Emails" onChange={(e) => handleChange(e)} type="email" placeholder="example@test.com" required />
-        </FloatingLabel>
-
-        <FloatingLabel>
-          <label>PASSWORD</label>
-          <input value={form.Password} name="Password" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
-        </FloatingLabel>
-
-        <FloatingLabel>
-          <label>CONFIRM-PASSWORD</label>
-          <input value={form.ConfirmPassword} name="ConfirmPassword" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
-        </FloatingLabel>
-
+        <label>Name</label>
+        <input value={form.Name} name="Name" onChange={(e) => handleChange(e)} type="text" required />
+        <label>Mobile No.</label>
+        <input value={form.Mobile} name="Mobile" onChange={(e) => handleChange(e)} type="number" required />
+        <label>Email</label>
+        <input value={form.Emails} name="Emails" onChange={(e) => handleChange(e)} type="email" required />
+        <label>Password</label>
+        <input value={form.Password} name="Password" onChange={(e) => handleChange(e)} type="password" required />
         <button type="submit">LOGIN</button>
       </div>
-      <a href="https://learnings12654600.blogspot.com/2019/12/">MADE BY JAYESH</a>
     </div>
   );
 }
